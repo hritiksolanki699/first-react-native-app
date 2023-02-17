100f18abbfcd7a852db7f06b252532c4ca5cc4a7
Hritik Solanki <hritiksolanki699@gmail.com>
2023-02-17 16:49:29 +0530

    first commit

---
+++ components/index.js
@@ -0,0 +1,19 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import DetailsDesc from "./DetailsDesc";
+import { CircleButton, RectButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import { NFTTitle, EthPrice, ImageCmp, People, SubInfo } from "./SubInfo";
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  NFTTitle,
+  SubInfo,
+  DetailsDesc,
+  DetailsBid
+};
